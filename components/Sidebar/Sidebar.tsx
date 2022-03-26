93d8187ad61dba6c7cbe42be3e23a4c808c338bd
Miyahara <haramiya21@gmail.com>
2022-03-26 22:39:50 +0900

    init page

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <>
+      <div className="fixed hidden h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
+        Sidebar
+      </div>
+    </>
+  )
+}
+
+export default Sidebar
